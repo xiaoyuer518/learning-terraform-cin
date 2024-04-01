c2a2d0c80703d36438bdcf0fca1dab605ad2e2b8
xiaoyuer518 <165121807+xiaoyuer518@users.noreply.github.com>
2024-03-31 23:16:58 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,8 +21,7 @@ data "aws_vpc" "default" {
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-
-vpc_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [aws_security_group.blog.id]
 
   tags = {
     Name = "Learning Terraform"
